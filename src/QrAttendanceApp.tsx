804acc02a5c2bc2aaa7e98f2dfed9f9011475b7d
limberMay <limbermay1@gmail.com>
2023-01-13 11:35:49 -0600

    Se retornó el componente del tema de la aplicación con las rutas principales

---
+++ src/QrAttendanceApp.tsx
@@ -0,0 +1,10 @@
+import {AppTheme} from "./theme";
+import {AppRouter} from "./routes/AppRouter";
+
+export const QrAttendanceApp = () => {
+    return (
+        <AppTheme>
+            <AppRouter />
+        </AppTheme>
+    )
+}
