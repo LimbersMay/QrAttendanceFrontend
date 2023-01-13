6cfd54e6d1a3a44ff9f75ccdadc3b6ba7938b49b
limberMay <limbermay1@gmail.com>
2023-01-13 15:02:17 -0600

    Se agregó el provedor de BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {QrAttendanceApp} from "./QrAttendanceApp";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <QrAttendanceApp />
+    <BrowserRouter>
+        <QrAttendanceApp />
+    </BrowserRouter>
   </React.StrictMode>,
 )
