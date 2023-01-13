38ba13ad60e5312a5a5b987c08d4f3f0c4fb3843
limberMay <limbermay1@gmail.com>
2023-01-13 12:34:13 -0600

    Se agregó el custom hook useForm con tipado de typescript

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,68 @@
+import React, {useEffect, useMemo, useState} from "react";
+
+interface FormValidation {
+    [key: string]: string | null;
+}
+
+interface FormValidations {
+    [key: string]: [Function, string];
+}
+
+interface FormState {
+    [key: string]: string;
+}
+
+export const useForm = ( initialForm: FormState, formValidations: FormValidations = {} ) => {
+
+    const [formState, setFormState] = useState<FormState>( initialForm );
+    const [ formValidation, setFormValidation ] = useState<FormValidation>({});
+
+    useEffect(() => {
+        createValidators();
+    }, [formState]);
+
+    const isFormValid = useMemo(() => {
+
+        for (const formValue of Object.keys(formValidation)) {
+            if (formValidation[formValue] !== null) return false;
+        }
+
+        return true;
+    }, [formValidation]);
+
+    const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+    }
+
+
+    const onResetForm = () => {
+        setFormState(initialForm);
+    }
+
+    const createValidators = () => {
+
+        const formCheckedValues: FormValidation = {};
+
+        for (const formField of Object.keys(formValidations)) {
+            const [ fn, errorMessage ] = formValidations[formField];
+
+            formCheckedValues[`${formField}Valid`] = fn(formState[formField]) ? null : errorMessage;
+        }
+
+        setFormValidation( formCheckedValues );
+    }
+
+    return {
+        ...formState,
+        formState,
+        onInputChange,
+        onResetForm,
+
+        ...formValidation,
+        isFormValid
+    }
+}
