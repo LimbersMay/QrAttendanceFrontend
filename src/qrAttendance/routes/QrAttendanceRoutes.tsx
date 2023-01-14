c5920c1348954ce5ee0f373cffdd7c0212fe677f
limberMay <limbermay1@gmail.com>
2023-01-13 18:03:11 -0600

    (QrAttendance) Componentes de la App principal agregado

---
+++ src/qrAttendance/routes/QrAttendanceRoutes.tsx
@@ -0,0 +1,11 @@
+import {Navigate, Route, Routes} from "react-router-dom";
+import {QrAttendancePage} from "../pages/QrAttendancePage";
+
+export const QrAttendanceRoutes = () => {
+    return (
+        <Routes>
+            <Route path='/' element={ <QrAttendancePage />} />
+            <Route path='/*' element={<Navigate to='/' />} />
+        </Routes>
+    )
+}
