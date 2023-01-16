8ea09169bcf01f4607eb7d999b49fd950bce9498
limberMay <limbermay1@gmail.com>
2023-01-16 15:48:07 -0600

    (qrAttendance) Se hizo uso del QrCode row en el componente de GroupViewTable

--- src/qrAttendance/views/GroupViewTable.tsx
+++ src/qrAttendance/views/GroupViewTable.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import {
     Box,
     Collapse,
-    IconButton,
     Table,
     TableBody,
     TableCell,
@@ -10,45 +9,14 @@ import {
     TableHead,
     TableRow,
     Typography,
-    Paper,
-    MenuItem,
-    Select,
-    SelectChangeEvent
+    Paper
 } from '@mui/material';
 
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-
-import {
-    DeleteOutline,
-    Delete,
-    EditOutlined,
-    Save,
-    Edit,
-    DownloadOutlined,
-    Download,
-    VisibilityOutlined,
-    Visibility
-} from '@mui/icons-material';
 import {useState} from "react";
-import {TextField} from "@mui/material";
-import {useForm} from "../../hooks/useForm";
-import {Registry} from "../components/Registry";
+import {RegistryRow} from "../components/RegistryRow";
+import {QrCodeRow} from "../components/QrCodeRow";
 
-interface registry {
-    date: string,
-    name: string,
-    sourname: string,
-    lastname: string
-}
-
-interface QrCode {
-    name: string,
-    registries: number,
-    date: string,
-    enabled: boolean,
-    history: registry[]
-}
+import {QrCode} from "../interfaces";
 
 const createData = (
     name: string,
@@ -82,120 +50,18 @@ const createData = (
     };
 }
 
-const Row = (props: { row: ReturnType<typeof createData> }) => {
+const Row = (props: { row: QrCode }) => {
 
     const { row } = props;
     const [open, setOpen] = useState(false);
 
-    const [ isEditing, setIsEditing ] = useState(false);
-    const [ registryEditId, setRegistryEditId ] = useState('');
-
-    const initialForm = {
-        name: row.name,
-        registries: row.registries,
-        date: row.date,
-        enabled: false,
-        id: ''
-    }
-
-    const { formState: qrCodeFormState, onInputChange } = useForm(initialForm);
-    const { name, registries, date } = qrCodeFormState;
-
-    const [isEnable, setIsEnable] = useState<boolean>(row.enabled);
-
-    const onSelectChange = (event: SelectChangeEvent) => {
-
-        const { value } = event.target;
-
-        if (value === 'YES') return setIsEnable(true);
-
-        setIsEnable(false);
-    }
-
-    const handleQrSave = () => {
-        setIsEditing(false);
-    }
-
-    const handleQrDelete = (row: ReturnType<typeof createData>) => {
-
-    }
-
-    const handleQrEdit = (row: ReturnType<typeof createData>) => {
-        setIsEditing(true);
-    }
-
-    const handleRegistrySave = (id: string) => {
-        setRegistryEditId('');
-    }
-
-    const handleRegistryEdit = (registryId: string) => {
-        setRegistryEditId(registryId);
-    }
-
-    const isEnabled = (enabled: boolean): string => {
-        return enabled ? 'YES' : 'NO';
+    const handleOpenSubTable = () => {
+        setOpen(!open);
     }
 
     return (
         <>
-            <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-                <TableCell>
-                    <IconButton
-                        aria-label="expand row"
-                        size="small"
-                        onClick={() => setOpen(!open)}
-                    >
-                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                    </IconButton>
-                </TableCell>
-                <TableCell component="th" scope="row">
-                    {
-                        isEditing
-                            ? <TextField name="name" onChange={onInputChange} value={name}></TextField>
-                            : name
-                    }
-                </TableCell>
-                <TableCell align="center">
-                    {
-                        isEditing
-                            ? <TextField sx={{width: '100px'}} name="registries" onChange={onInputChange} value={registries}></TextField>
-                            : registries
-                    }
-                </TableCell>
-                <TableCell align="center">
-                    {
-                        isEditing
-                            ? <TextField sx={{width: '140px'}} onChange={onInputChange} name="date" value={date}></TextField>
-                            : date
-                    }
-                </TableCell>
-                <TableCell align="center">
-                    {
-                        isEditing
-                            ? <Select value={isEnabled(isEnable)} onChange={onSelectChange}>
-                                <MenuItem value="YES">Yes</MenuItem>
-                                <MenuItem value="NO">No</MenuItem>
-                              </Select>
-                            : isEnabled(isEnable)
-                    }
-                </TableCell>
-                <TableCell align="center">
-                    <IconButton onClick={() => handleQrDelete(row)}>
-                        <Visibility />
-                    </IconButton>
-                    <IconButton onClick={() => handleQrDelete(row)}>
-                        <Download />
-                    </IconButton>
-                    {
-                        isEditing
-                            ? <IconButton onClick={handleQrSave}><Save /></IconButton>
-                            : <IconButton onClick={() => handleQrEdit(row)}><Edit /></IconButton>
-                    }
-                    <IconButton onClick={() => handleQrDelete(row)}>
-                        <Delete />
-                    </IconButton>
-                </TableCell>
-            </TableRow>
+            <QrCodeRow qrCodeRow={row} handleOpenSubTable={handleOpenSubTable} open={open}/>
 
             <TableRow>
                 <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
@@ -216,7 +82,7 @@ const Row = (props: { row: ReturnType<typeof createData> }) => {
                                 </TableHead>
                                 <TableBody>
                                     {row.history.map((historyRow) => (
-                                        <Registry key={historyRow.id} registryRow={historyRow} />
+                                        <RegistryRow key={historyRow.id} registryRow={historyRow} />
                                     ))}
                                 </TableBody>
                             </Table>
@@ -230,7 +96,7 @@ const Row = (props: { row: ReturnType<typeof createData> }) => {
 
 const rows = [
     createData('Virtual session', 10, '2023-05-12',true, 'nfjsd3454'),
-    createData('face-to-face session', 5, '2022-01-12',false, '2n4n3k3'),
+    createData('face-to-face session', 5, '2022-01-12',false, '2n4n3k3')
 ];
 
 export const GroupViewTable = () => {
