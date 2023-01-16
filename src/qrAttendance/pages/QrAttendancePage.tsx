3b653b4c7a9a4ecd34e7ec1e78089de05983f483
limberMay <limbermay1@gmail.com>
2023-01-16 15:48:59 -0600

    (qrAttendance) Se renderizó el componente de GroupView

--- src/qrAttendance/pages/QrAttendancePage.tsx
+++ src/qrAttendance/pages/QrAttendancePage.tsx
@@ -1,8 +1,7 @@
 import {QrAttendanceLayout} from "../layout/QrAttendanceLayout";
-import {NothingSelectedView} from "../views";
 import {IconButton} from "@mui/material";
 import {AddOutlined} from "@mui/icons-material";
-import {NoteView} from "../views/NoteView";
+import {GroupViewTable} from "../views";
 
 export const QrAttendancePage = () => {
 
@@ -18,8 +17,7 @@ export const QrAttendancePage = () => {
             { /*<Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum commodo nisl sit amet nulla accumsan dapibus. Nulla imperdiet sapien eget lorem condimentum accumsan. Duis volutpat sem ultricies imperdiet commodo. Vivamus vitae vehicula nibh. Nam sodales nisl sit amet odio aliquet, quis efficitur arcu vehicula. Curabitur molestie sagittis odio in fringilla. Duis molestie, libero id euismod pretium, mauris augue euismod ligula, id sagittis dolor ipsum eu libero. Nullam condimentum leo ipsum, a molestie enim venenatis in. Suspendisse potenti. Ut malesuada finibus molestie. Nunc faucibus ligula purus, ac gravida nunc varius eu. Donec in justo et lacus blandit dignissim quis id libero. Sed turpis libero, gravida a efficitur eget, aliquet at eros. Ut laoreet tempor viverra. Curabitur vestibulum justo at sem vehicula iaculis. Fusce ipsum purus, ultricies sodales erat id, auctor semper lectus.</Typography> */}
 
             {/*<NothingSelectedView />*/}
-            {/*<NoteView.tsx />*/}
-            <NoteView />
+            <GroupViewTable />
 
             {/*{*/}
             {/*    (!!active)*/}
