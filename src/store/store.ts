e2030cf8a8da23c5af6cffa0f158d04fb53ceb1e
limberMay <limbermay1@gmail.com>
2023-01-18 15:29:01 -0600

    (store) Reducer del authSlice agregado al store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {counterSlice} from "./counter";
+import {authSlice} from "./auth";
 export const store = configureStore({
     reducer: {
-
+        auth: authSlice.reducer
     }
 });
 
