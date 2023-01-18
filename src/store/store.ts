22abc7d36922e22b2f7f9334d8daecc0ab2e042c
limberMay <limbermay1@gmail.com>
2023-01-18 17:32:28 -0600

    (store) slice de qrAttendance agregado

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import {authSlice} from "./auth";
+import {qrAttendanceSlice} from "./qrAttendance";
 export const store = configureStore({
     reducer: {
-        auth: authSlice.reducer
+        auth: authSlice.reducer,
+        qrAttendance: qrAttendanceSlice.reducer
     }
 });
 
