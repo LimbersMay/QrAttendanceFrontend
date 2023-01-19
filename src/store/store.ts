7901dd93fadc326766f9d47bfc86086c6dcef6c3
limberMay <limbermay1@gmail.com>
2023-01-18 19:28:22 -0600

    (store) Se exportó el type AppThunk para los thunks

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {AnyAction, configureStore, ThunkAction} from '@reduxjs/toolkit';
 import {authSlice} from "./auth";
 import {qrAttendanceSlice} from "./qrAttendance";
 export const store = configureStore({
@@ -12,3 +12,10 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootState,
+    unknown,
+    AnyAction
+>
