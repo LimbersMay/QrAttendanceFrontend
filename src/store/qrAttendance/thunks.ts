a0b80540e79369c445c17425d0c7d01addb85a47
limberMay <limbermay1@gmail.com>
2023-01-19 09:36:03 -0600

    (store/qrAttendance) Crud de Group agregado

--- src/store/qrAttendance/thunks.ts
+++ src/store/qrAttendance/thunks.ts
@@ -1,13 +1,43 @@
 import {AppThunk} from "../store";
-import {removeQrCodeById} from "./qrAttendanceSlice";
-import {QrCode} from "../../qrAttendance/interfaces";
+import {addEmtpyQrCode, removeQrCodeById, updateGroup, updateQrCode} from "./qrAttendanceSlice";
+import {Group, QrCode} from "../../qrAttendance/interfaces";
 
-export const startRemovingQrCodeById = (qrCodeId: string): AppThunk => {
-    return async(dispatch) => {
+export const startRemoveQrCode = (qrCodeId: string): AppThunk => {
+    return async (dispatch) => {
 
         // Async code here
 
         // sync code here
         dispatch(removeQrCodeById(qrCodeId));
+    }
+}
+
+export const startAddQrCodeToGroup = (qrCode: QrCode): AppThunk => {
+    return async (dispatch, getState) => {
+
+        // Async code here
+
+        // sync code here
+        dispatch(updateQrCode(qrCode));
+    }
+}
+
+export const startSaveGroup = (group: Group): AppThunk => {
+    return async (dispatch) => {
+
+        // Async code here
+
+        // sync code here
+        dispatch(updateGroup(group));
+    }
+}
+
+export const startAddEmptyQrCode = (qrCode: QrCode): AppThunk => {
+    return async (dispatch) => {
+
+        // Async code here
+
+        // sync code here
+        dispatch(addEmtpyQrCode(qrCode));
     }
 }
