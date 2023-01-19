1a5cfa208943e60545a49d940096f220dd84a805
limberMay <limbermay1@gmail.com>
2023-01-19 17:43:35 -0600

    (slice/registry) acciones de deleteRegistry y updateRegistry agregadas

--- src/store/qrAttendance/registry/registrySlice.ts
+++ src/store/qrAttendance/registry/registrySlice.ts
@@ -2,24 +2,44 @@ import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {Registry} from "../../../qrAttendance/interfaces";
 import {RootState} from "../../store";
 
+const registries: Registry[] = [
+    {
+        id: new Date().toISOString() + '1',
+        qrCodeId: '344',
+        date: new Date().toUTCString(),
+        name: 'Juan',
+        sourname: 'Hau',
+        lastname: 'Diaz'
+    }
+]
+
 interface RegistryState {
     registries: Registry[];
 }
 
 const initialState: RegistryState = {
-    registries: []
+    registries: registries
 }
 
 export const registrySlice = createSlice({
     name: 'registry',
     initialState,
     reducers: {
-        addEmptyRegistry: (state, action: PayloadAction<Registry>) => {
+        addNewRegistry: (state, action: PayloadAction<Registry>) => {
             state.registries.push(action.payload);
         },
+        deleteRegistry: (state, action: PayloadAction<string>) => {
+            state.registries = state.registries.filter(registry => registry.id !== action.payload);
+        },
+        updateRegistry: (state, action: PayloadAction<Registry>) => {
+            state.registries = state.registries.map(registry => {
+                if (registry.id === action.payload.id) return action.payload;
+                return registry;
+            })
+        }
     }
 });
 
 // Action creators are generated for each case reducer function
-export const { addEmptyRegistry } = registrySlice.actions;
+export const { addNewRegistry, deleteRegistry, updateRegistry } = registrySlice.actions;
 export const selectRegistry = (state: RootState) => state.registry;
