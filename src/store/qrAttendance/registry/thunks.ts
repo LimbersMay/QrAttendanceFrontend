ef5d7c5c470a6cf8033b9056e8c1e03d33b8ab88
limberMay <limbermay1@gmail.com>
2023-01-19 17:56:41 -0600

    (thunk/registry) thunk startUpdateRegistry implementado

--- src/store/qrAttendance/registry/thunks.ts
+++ src/store/qrAttendance/registry/thunks.ts
@@ -1,4 +1,24 @@
+import {AppThunk} from "../../store";
+import {Registry} from "../../../qrAttendance/interfaces";
+import {updateRegistry} from "./registrySlice";
 
-export const name = () => {
+export const startAddNewRegistry = (): AppThunk => {
+    return async(dispatch) => {
 
+    }
+}
+
+export const startDeleteRegistry = (): AppThunk => {
+    return async(dispatch) => {
+
+    }
+}
+
+export const startUpdateRegistry = (registry: Registry): AppThunk => {
+    return async(dispatch) => {
+        // async code here
+
+        // sync code here
+        dispatch(updateRegistry(registry));
+    }
 }
