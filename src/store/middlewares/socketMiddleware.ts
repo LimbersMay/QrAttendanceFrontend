d875c2070d22ca88b29cfd4d1cdcdb7695e1f5ce
limberMay <limbermay1@gmail.com>
2023-01-21 13:32:28 -0600

    (store/middleware) middleware para la conexión entre el socket creado

---
+++ src/store/middlewares/socketMiddleware.ts
@@ -0,0 +1,21 @@
+import { Middleware, Dispatch, AnyAction } from 'redux';
+import io from 'socket.io-client';
+import { addNewRegistry } from '../qrAttendance';
+
+import {Registry} from "../../qrAttendance/interfaces";
+
+const socketMiddleware: Middleware = (store) => {
+    const socket = io('http://localhost:5173');
+    const handleNewRegistries = (data: Registry) => {
+        store.dispatch(addNewRegistry(data));
+    }
+    socket.on('new-registries', (data: any) => {
+        handleNewRegistries(data);
+    });
+    return (next: Dispatch) => (action: AnyAction) => {
+        next(action);
+    }
+}
+
+export default socketMiddleware;
+
