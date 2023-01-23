eb567598d4863f0a6f3c91e12f4ea869459e909c
limberMay <limbermay1@gmail.com>
2023-01-23 11:13:56 -0600

    (store/auth) thunk para registrar usuario implementado con backend

---
+++ src/store/auth/thunks.ts
@@ -0,0 +1,27 @@
+import {AppThunk} from "../store";
+import {checkingCredentials, login} from "./authSlice";
+import {qrAttendanceApi} from "../../api/qrAttendanceApi";
+
+export const startCreatingUser = ({name, email, sourname, lastname, password}: {name: string, email: string, sourname: string, lastname: string, password: string}): AppThunk => {
+    return async (dispatch, getState) => {
+        // async code here
+        dispatch(checkingCredentials());
+
+        const response = await qrAttendanceApi.post('/api/user/register', {
+            name,
+            email,
+            fathersName: sourname,
+            mothersName: lastname,
+            password
+        });
+
+        const { user } = response.data;
+
+        // sync code here
+        dispatch(login({
+            uid: user.id,
+            displayName: user.name,
+            email: user.email,
+        }));
+    }
+}
