bfe6ae8753a54896dbc96bd810e7d7bae33e0ac0
limberMay <limbermay1@gmail.com>
2023-01-23 15:04:11 -0600

    (hooks) hook personalizado useCheckAuth.ts agregado para verificar si un usuario se encuentra actualmente autenticado

---
+++ src/hooks/useCheckAuth.ts
@@ -0,0 +1,34 @@
+import {useSelector} from "react-redux";
+import {login, logout, selectAuth} from "../store/auth";
+import {useAppDispatch} from "../store";
+import {useEffect} from "react";
+import {qrAttendanceApi} from "../api/qrAttendanceApi";
+
+export const useCheckAuth = () => {
+
+    const { status } = useSelector(selectAuth);
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+
+        qrAttendanceApi.get("/auth/authenticated", {
+            withCredentials: true
+        }).then((response) => {
+            const { user } = response.data;
+
+            if (!user) {
+                return dispatch(logout(null));
+            }
+
+            const { id, name, email } = user;
+            dispatch(login({
+                uid: id,
+                displayName: name,
+                email: email,
+            }));
+        });
+
+    }, []);
+
+    return status;
+}
