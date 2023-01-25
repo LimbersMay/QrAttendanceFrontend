b4a607fe6bf6f8abb584b0ea9cbd3cd48b277518
limberMay <limbermay1@gmail.com>
2023-01-25 13:14:09 -0600

    (qrAttendance/errors) Error de credenciales inválidas agregada al getValidationError

--- src/utilities/get-validation-error.ts
+++ src/utilities/get-validation-error.ts
@@ -2,8 +2,9 @@ import {TypeWithKey} from "../models";
 
 export const getValidationError = (errorCode: string) => {
     const codeMatcher: TypeWithKey<string> = {
-        ERR_NETWORK: "NETWORK ERROR",
-        GROUP_NOT_FOUND: "GROUP NOT FOUND",
+        ERR_NETWORK: 'NETWORK ERROR',
+        GROUP_NOT_FOUND: 'GROUP NOT FOUND',
+        INVALID_CREDENTIALS: 'INVALID CREDENTIALS',
     }
 
     return codeMatcher[errorCode] || "Something went wrong";
