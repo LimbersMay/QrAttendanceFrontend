2bcacff43081dd1356b923ce28d466582701d9b5
limberMay <limbermay1@gmail.com>
2023-01-25 13:59:07 -0600

    CRUD completo de groups implementado fixes #6

--- src/qrAttendance/pages/QrAttendancePage.tsx
+++ src/qrAttendance/pages/QrAttendancePage.tsx
@@ -19,10 +19,6 @@ export const QrAttendancePage = () => {
 
     return (
         <QrAttendanceLayout>
-            { /*<Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum commodo nisl sit amet nulla accumsan dapibus. Nulla imperdiet sapien eget lorem condimentum accumsan. Duis volutpat sem ultricies imperdiet commodo. Vivamus vitae vehicula nibh. Nam sodales nisl sit amet odio aliquet, quis efficitur arcu vehicula. Curabitur molestie sagittis odio in fringilla. Duis molestie, libero id euismod pretium, mauris augue euismod ligula, id sagittis dolor ipsum eu libero. Nullam condimentum leo ipsum, a molestie enim venenatis in. Suspendisse potenti. Ut malesuada finibus molestie. Nunc faucibus ligula purus, ac gravida nunc varius eu. Donec in justo et lacus blandit dignissim quis id libero. Sed turpis libero, gravida a efficitur eget, aliquet at eros. Ut laoreet tempor viverra. Curabitur vestibulum justo at sem vehicula iaculis. Fusce ipsum purus, ultricies sodales erat id, auctor semper lectus.</Typography> */}
-
-            {/*<NothingSelectedView />*/}
-            {/*<GroupViewTable/>*/}
 
             {
                 (!!active)
