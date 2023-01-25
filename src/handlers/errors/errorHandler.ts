b7739ef1bc7c81bfa51bd47cca4bccf0157e8f9a
limberMay <limbermay1@gmail.com>
2023-01-25 12:47:14 -0600

    (qrAttendance) Clase para manejar excepciones con sweetAlert eliminada

--- src/handlers/errors/errorHandler.ts
+++
@@ -1,9 +0,0 @@
-
-import Swal from "sweetalert2";
-import 'sweetalert2/dist/sweetalert2.css';
-
-export class ErrorHandler {
-    public static handleError(code: string): void {
-        Swal.fire('Error', code, 'error').then()
-    }
-}
