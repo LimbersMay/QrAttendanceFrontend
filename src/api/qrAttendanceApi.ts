644d9c583b91bb9d5dfbe4dffba79c7189d3e940
limberMay <limbermay1@gmail.com>
2023-01-24 23:07:32 -0600

    (qrAttendance/user) se agregaron las credenciales para todas las peticiones hechas con el qrAttendanceApi de axios

--- src/api/qrAttendanceApi.ts
+++ src/api/qrAttendanceApi.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const qrAttendanceApi = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: 'http://localhost:3000/api',
+    withCredentials: true
 });
