b3a31faeb1a4f466b44e7c01bfd5e3910b9b5988
limberMay <limbermay1@gmail.com>
2023-01-25 12:47:49 -0600

    (qrAttendance) interceptor de axios agregado para manejar las exepciones realizadas a la qrAttendanceApi.ts

---
+++ src/interceptors/axios.interceptor.ts
@@ -0,0 +1,28 @@
+import {getValidationError} from "../utilities";
+import {SnackbarUtilities} from "../utilities/snackbar-manager";
+import {qrAttendanceApi} from "../api/qrAttendanceApi";
+
+export const AxiosInterceptor = () => {
+    qrAttendanceApi.interceptors.request.use((request) => {
+        request.withCredentials = true;
+
+        return request;
+    });
+
+    qrAttendanceApi.interceptors.response.use(
+        (response) => {
+            return response;
+        },
+        (error) => {
+
+            if (error.response) {
+                SnackbarUtilities.error(getValidationError(error.response.data.msg));
+            }
+
+            if (!error.response) {
+                SnackbarUtilities.error(getValidationError('ERR_NETWORK'));
+            }
+            return Promise.reject(error);
+        }
+    );
+}
