0eedf7a48393489c9bf18dd71bba2b2cf866a3de
limberMay <limbermay1@gmail.com>
2023-01-31 14:50:39 -0600

    (qrAttendance) atributo registries eliminado de la intefaz QrCode.ts

--- src/qrAttendance/interfaces/QrCode.ts
+++ src/qrAttendance/interfaces/QrCode.ts
@@ -1,10 +1,7 @@
-import {Registry} from "./Registry";
-
 export interface QrCode {
     id: string,
     groupId: string,
     name: string,
-    registries: number,
     date: string,
     enabled: boolean
 }
