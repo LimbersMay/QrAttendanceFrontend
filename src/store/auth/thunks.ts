8e1c8be98a16da56c73918ec35b3782141fbcc39
limberMay <limbermay1@gmail.com>
2023-01-31 16:18:56 -0600

    (qrAttendance/qrCode) se reseteó todo el store al hacer un logout

--- src/store/auth/thunks.ts
+++ src/store/auth/thunks.ts
@@ -1,6 +1,7 @@
 import {AppThunk} from "../store";
 import {checkingCredentials, login, logout} from "./authSlice";
 import {qrAttendanceApi} from "../../api/qrAttendanceApi";
+import {setGroups, setQrCodes, setRegistries} from "../qrAttendance";
 
 export const startCreatingUser = ({name, email, lastname, password}: {name: string, email: string, lastname: string, password: string}): AppThunk => {
     return async (dispatch) => {
@@ -72,5 +73,8 @@ export const startLogout = (): AppThunk => {
         });
 
         dispatch(logout(null));
+        dispatch(setGroups([]));
+        dispatch(setQrCodes([]));
+        dispatch(setRegistries([]));
     }
 }
