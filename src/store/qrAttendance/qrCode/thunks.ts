7c690a03ccb985b531ad82d0f5f2cfd75c499897
limberMay <limbermay1@gmail.com>
2023-01-31 14:47:05 -0600

    (qrAttendance/store/qrCode) thunks para cargar, actualizar y crear un nuevo qrCode implementado

--- src/store/qrAttendance/qrCode/thunks.ts
+++ src/store/qrAttendance/qrCode/thunks.ts
@@ -1,41 +1,67 @@
 import {AppThunk} from "../../store";
 import {QrCode} from "../../../qrAttendance/interfaces";
-import {addEmptyQrCode, deleteQrCode, updateQrCode} from "./qrCodeSlice";
+import {addEmptyQrCode, setQrCodes, updateQrCode} from "./qrCodeSlice";
+import {qrAttendanceApi} from "../../../api/qrAttendanceApi";
+
+export const startLoadingQrCodes = (): AppThunk => {
+    return async (dispatch) => {
+        // async code here
+        const response = await qrAttendanceApi.get('/qrCode/all');
+        const {body} = response.data;
+
+        const qrCodes: QrCode[] = body.map((qrCode: any) => {
+            return {
+                id: qrCode.id,
+                groupId: qrCode.groupId,
+                name: qrCode.name,
+                date: qrCode.createdAt,
+                enabled: qrCode.enabled
+            }
+        });
+
+        // sync code here
+        dispatch(setQrCodes(qrCodes));
+    }
+}
 
 export const startNewQrCode = (groupId: string): AppThunk => {
-    return async(dispatch) => {
+    return async (dispatch) => {
 
-        const newQrCode: QrCode = {
-            id: '',
+        const response = await qrAttendanceApi.post('/qrCode/create', {
             groupId: groupId,
             name: 'Default',
-            date: '2023-01-18',
-            registries: 0,
+            enabled: false
+        });
+
+        const {body} = response.data;
+
+        const newQrCode: QrCode = {
+            groupId: groupId,
+            id: body.id,
+            name: body.name,
+            date: body.createdAt,
             enabled: false
         }
-
-        // async code here
-        // get the id from the backend
-        newQrCode.id = new Date().toISOString();
 
         // sync code here
         dispatch(addEmptyQrCode(newQrCode));
     }
 }
 
-export const startDeleteQrCode = (qrCodeId: string): AppThunk => {
-    return async(dispatch) => {
-        // async code here
-
-        // sync code here
-        dispatch(deleteQrCode(qrCodeId));
-    }
-}
-
 export const startUpdateQrCode = (qrCode: QrCode): AppThunk => {
-    return async(dispatch) => {
+    return async (dispatch) => {
 
         // async code here
+        const {id, name, enabled, date} = qrCode;
+
+        await qrAttendanceApi.put('/qrCode/update', {
+            id: id,
+            updatedFields: {
+                name,
+                enabled,
+                updatedAt: date
+            }
+        });
 
         // sync code here
         dispatch(updateQrCode(qrCode));
