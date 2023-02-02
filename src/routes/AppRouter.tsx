bfab1bf87618784c5238af372e71ab2d0a79af96
limberMay <limbermay1@gmail.com>
2023-02-01 18:08:06 -0600

    (qrAttendance/qrCode) Ruta del formulario de registro de asistencia agregado

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -4,6 +4,7 @@ import {QrAttendanceRoutes} from "../qrAttendance/routes/QrAttendanceRoutes";
 import {useCheckAuth} from "../hooks/useCheckAuth";
 import {authStatusTypes} from "../auth/types";
 import {CheckingAuth} from "../iu";
+import {CheckInFormRouter} from "../checkInForm/CheckInFormRouter";
 
 export const AppRouter = () => {
 
@@ -19,9 +20,13 @@ export const AppRouter = () => {
             {
                 status === authStatusTypes.authenticated
                     ? <Route path="/*" element={<QrAttendanceRoutes />}/>
-                    : <Route path="/auth/*" element={<AuthRoutes />}/>
+                    : <>
+                        <Route path="/auth/*" element={<AuthRoutes />}/>
+                        <Route path="/checkIn/*" element={<CheckInFormRouter />} />
+                    </>
             }
 
+            <Route path="/checkIn/*" element={<CheckInFormRouter />} />
             <Route path="/*" element={<Navigate to="/auth/login" />}/>
 
         </Routes>
