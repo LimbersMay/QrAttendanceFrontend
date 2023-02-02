e004d0bcc2dfdf8e68379dc03ff128d408ede4f3
limberMay <limbermay1@gmail.com>
2023-02-02 12:04:56 -0600

    (checkingForm) socketMiddleware.ts para registrar los eventos del socket implementado

--- src/store/middlewares/socketMiddleware.ts
+++ src/store/middlewares/socketMiddleware.ts
@@ -1,17 +1,7 @@
 import { Middleware, Dispatch, AnyAction } from 'redux';
-import io from 'socket.io-client';
-import { addNewRegistry } from '../qrAttendance';
-
-import {Registry} from "../../qrAttendance/interfaces";
 
 const socketMiddleware: Middleware = (store) => {
-    const socket = io('http://localhost:3000');
-    const handleNewRegistries = (data: Registry) => {
-        store.dispatch(addNewRegistry(data));
-    }
-    socket.on('new-registries', (data: any) => {
-        handleNewRegistries(data);
-    });
+
     return (next: Dispatch) => (action: AnyAction) => {
         next(action);
     }
