612406c2416dcae163ccd5884b193348e073001d
limberMay <limbermay1@gmail.com>
2023-02-01 18:55:46 -0600

    (qrAttendance/qrCode) componente para mostrar el ćodigo qr generador, agregado

---
+++ src/qrAttendance/components/qrCodeRow/QrCheckIn.tsx
@@ -0,0 +1,35 @@
+import {Box, Dialog, DialogContent, DialogTitle, IconButton} from "@mui/material";
+import {Close} from "@mui/icons-material";
+import QRCode from 'react-qr-code';
+
+export const QrCheckIn = ({ url, title, isQrShowing, handleToggleShowQr }: { url: string, title: string, isQrShowing: boolean, handleToggleShowQr: any}) => {
+    return (
+        <Dialog open={isQrShowing} maxWidth="sm" fullWidth>
+            <Box
+                display="flex"
+                justifyContent="center"
+            >
+                <DialogTitle>{ title } QR Code</DialogTitle>
+            </Box>
+            <Box position="absolute" top={0} right={0}>
+                <IconButton onClick={handleToggleShowQr}>
+                    <Close />
+                </IconButton>
+            </Box>
+            <DialogContent>
+                <Box
+                    sx={{height: 'auto', margin: "0 auto", maxWidth: "64", width: "100%"}}
+                    display="flex"
+                    justifyContent="center"
+                >
+                    <QRCode
+                        size={100}
+                        style={{ height: "auto", maxWidth: "50%", width: "80%" }}
+                        value={"google.com"}
+                        viewBox={`0 0 256 256`}
+                    />
+                </Box>
+            </DialogContent>
+        </Dialog>
+    );
+}
