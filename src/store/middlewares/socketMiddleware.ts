bad8c53a434a9961ff288f64b0270cdc411d3bca
limberMay <limbermay1@gmail.com>
2023-02-02 21:13:21 -0600

    (qrAttendance/registry) conexión con sockets implementada en el middleware de redux

--- src/store/middlewares/socketMiddleware.ts
+++ src/store/middlewares/socketMiddleware.ts
@@ -1,6 +1,23 @@
 import { Middleware, Dispatch, AnyAction } from 'redux';
+import socket from "../../utilities/socketIo";
+import {Registry} from "../../qrAttendance/interfaces";
+import {addNewRegistry} from "../qrAttendance";
+import {SnackbarUtilities} from "../../utilities/snackbar-manager";
+import {getValidationError} from "../../utilities";
 
 const socketMiddleware: Middleware = (store) => {
+
+    socket.on("new-attendance", (data: Registry) => {
+        store.dispatch(addNewRegistry(data));
+    });
+
+    socket.on("register-attendance-error", (message: string) => {
+        SnackbarUtilities.error(getValidationError(message));
+    });
+
+    socket.on("register-attendance-success", () => {
+        SnackbarUtilities.sucess('Attendance registered successfully');
+    });
 
     return (next: Dispatch) => (action: AnyAction) => {
         next(action);
