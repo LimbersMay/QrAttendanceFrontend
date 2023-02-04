d890d23344eb96a6a8d05a9130163dcbee4fac6b
limberMay <limbermay1@gmail.com>
2023-02-04 17:33:48 -0600

    (contexts/qrAttendance)protocolos agregados a los sockets

--- src/utilities/socketIo.ts
+++ src/utilities/socketIo.ts
@@ -1,5 +1,8 @@
 
 import io from 'socket.io-client';
-const socket = io('https://qrattendancebackend.up.railway.app:8004');
+const socket = io('https://qrattendancebackend.up.railway.app:8004', {
+    withCredentials: true,
+    protocols: ['websocket', 'polling', 'flashsocket'],
+});
 
 export default socket;
