4861decc0cf570906593dfe2551058e85f03b858
limberMay <limbermay1@gmail.com>
2023-02-07 10:58:03 -0600

    (qrAttendance) campos group y career agregados al FormType

--- src/checkInForm/types/formType.ts
+++ src/checkInForm/types/formType.ts
@@ -3,6 +3,8 @@ import {Socket} from "socket.io-client";
 export interface FormType {
     io: Socket;
     name: string;
+    group: string;
+    career: string;
     firstSurname: string;
     secondSurname: string;
     formId: string;
