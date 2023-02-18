ae84e3e604ab17d1255f78b4c85a948ef45621d4
limberMay <limbermay1@gmail.com>
2023-02-17 21:19:44 -0600

    (CheckInFormPage) Campo career manejado como un select

--- src/qrAttendance/components/RegistryRow/RegistryRow.tsx
+++ src/qrAttendance/components/RegistryRow/RegistryRow.tsx
@@ -1,9 +1,9 @@
-import {useState} from "react";
+import React, {useState} from "react";
 
 import {
     TableCell,
     IconButton,
-    TableRow
+    TableRow, MenuItem, Select, SelectChangeEvent
 } from "@mui/material";
 import {Save} from "@mui/icons-material";
 
@@ -16,15 +16,17 @@ import {RegistryRowMenuOptions} from "./RegistryMenuOptions";
 import {RegistryDatePicker} from "./RegistryDatePicker";
 import dayjs from "dayjs";
 
-export const RegistryRow = ({registryRow }: { registryRow: Registry}) => {
+export const RegistryRow = ({registryRow}: { registryRow: Registry }) => {
 
     const dispatch = useAppDispatch();
 
     const [isRowEditing, setIsRowEditing] = useState(false);
     const {formState, onInputChange} = useForm(registryRow);
 
-    const { name, group, career, firstSurname, secondSurname } = formState;
-    const [ date, setDate ] = useState(registryRow.checkinTime);
+    const {name, group, firstSurname, secondSurname} = formState;
+
+    const [date, setDate] = useState<string>(registryRow.checkinTime);
+    const [career, setCareer] = useState<string>(registryRow.career);
 
     const handleEdit = () => {
         setIsRowEditing(true);
@@ -34,12 +36,18 @@ export const RegistryRow = ({registryRow }: { registryRow: Registry}) => {
         setDate(date);
     }
 
+    const onSelectChange = (event: SelectChangeEvent) => {
+        setCareer(event.target.value);
+    }
+
     const handleSave = () => {
         dispatch(startUpdateRegistry({
             ...registryRow,
             ...formState,
-            checkinTime: date
+            checkinTime: date,
+            career: career
         }));
+
         setIsRowEditing(false);
     }
 
@@ -53,7 +61,7 @@ export const RegistryRow = ({registryRow }: { registryRow: Registry}) => {
 
                 {
                     isRowEditing
-                        ? <RegistryDatePicker date={date} onChangeDate={onChangeDate} />
+                        ? <RegistryDatePicker date={date} onChangeDate={onChangeDate}/>
                         : dayjs(date).format('MMMM D, YYYY h:mm A')
                 }
 
@@ -67,12 +75,18 @@ export const RegistryRow = ({registryRow }: { registryRow: Registry}) => {
                 />
             </TableCell>
             <TableCell align="center">
-                <ConditionalTextField
-                    name="career"
-                    value={career}
-                    onChange={onInputChange}
-                    condition={isRowEditing}
-                />
+                {
+                    isRowEditing
+                        ? <Select fullWidth variant={'outlined'} value={career} onChange={onSelectChange}>
+                            <MenuItem value="Computer Systems Engineering">Computer Systems Engineering</MenuItem>
+                            <MenuItem value="Civil Engineering">Civil Engineering</MenuItem>
+                            <MenuItem value="Environmental Engineering">Environmental Engineering</MenuItem>
+                            <MenuItem value="Industrial Engineering">Industrial Engineering</MenuItem>
+                            <MenuItem value="Engineering in Administration">Engineering in Administration</MenuItem>
+                            <MenuItem value="Bussines Management Engineering">Bussines Management Engineering</MenuItem>
+                        </Select>
+                        : career
+                }
             </TableCell>
             <TableCell align="center" sx={{display: {xs: 'none', sm: 'table-cell'}}}>
                 <ConditionalTextField
@@ -104,7 +118,7 @@ export const RegistryRow = ({registryRow }: { registryRow: Registry}) => {
                 {
                     isRowEditing
                         ? <IconButton onClick={handleSave}>
-                            <Save />
+                            <Save/>
                         </IconButton>
                         : <RegistryRowMenuOptions
                             handleEdit={handleEdit}
