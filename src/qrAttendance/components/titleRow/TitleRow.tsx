0f130d7483dccb3d1249426e36d69b5e0a816dd1
limberMay <limbermay1@gmail.com>
2023-02-19 15:15:47 -0600

    (hooks) se manejó el edit del group en el title desde el TitleModal.tsx

--- src/qrAttendance/components/titleRow/TitleRow.tsx
+++ src/qrAttendance/components/titleRow/TitleRow.tsx
@@ -1,45 +1,34 @@
 import React, {useMemo, useState} from "react";
 
 import {IconButton, TableCell, tableCellClasses, TableRow, TextField, Tooltip} from "@mui/material";
-import {AddOutlined, SearchOutlined, EditOutlined, SaveOutlined, DeleteOutlined} from "@mui/icons-material";
+import {AddOutlined, SearchOutlined, EditOutlined, DeleteOutlined} from "@mui/icons-material";
 import {useForm} from "../../../hooks/useForm";
 
-import {ConditionalTextField} from "../ConditionalTextField";
 import {Group, QrCode} from "../../interfaces";
 import ConfirmDialog from "./ConfirmDialog";
 import {SnackbarUtilities} from "../../../utilities/snackbar-manager";
-import {useGroupSlice} from "../../../hooks/useGroupSlice";
 import {useQrAttendanceSlice} from "../../../hooks/useQrAttendanceSlice";
 import {useQrCodeSlice} from "../../../hooks/useQrCodeSlice";
+import {useUiSlice} from "../../../hooks/useUiSlice";
+import {TitleModal} from "./TitleModal";
 
-export const TitleRow = React.memo(({ group, qrCodes }: {group: Group, qrCodes: QrCode[]}) => {
+export const TitleRow = React.memo(({group, qrCodes}: { group: Group, qrCodes: QrCode[] }) => {
 
     const initialStateForm = useMemo(() => ({
         groupTitle: group.name,
     }), [group]);
 
-    const { updateGroup } = useGroupSlice();
-    const { handleStartNewQrCode } = useQrCodeSlice();
-    const { deleteGroupWithDependencies } = useQrAttendanceSlice();
+    const { toggleTitleModal } = useUiSlice();
+    const {handleStartNewQrCode} = useQrCodeSlice();
+    const {deleteGroupWithDependencies} = useQrAttendanceSlice();
 
-    const { formState, onInputChange } = useForm(initialStateForm);
-
-    const [isRowEditing, setIsRowEditing] = useState<boolean>(false);
+    const {formState} = useForm(initialStateForm);
     const [isTryingToDelete, setIsTryingToDelete] = useState<boolean>(false);
 
-    const { groupTitle } = formState;
-
-    const handleEditRow = () => {
-        setIsRowEditing(true);
-    }
+    const {groupTitle} = formState;
 
     const handleToggleDeleteDialog = () => {
         setIsTryingToDelete(!isTryingToDelete);
-    }
-
-    const handleSaveRow = () => {
-        setIsRowEditing(false);
-        updateGroup({ ...group, name: groupTitle});
     }
 
     const handleDeleteRow = () => {
@@ -52,74 +41,69 @@ export const TitleRow = React.memo(({ group, qrCodes }: {group: Group, qrCodes: 
     }
 
     return (
-        <TableRow
-            sx={{
-                [`& .${tableCellClasses.root}`]: {
-                    borderBottom: "none"
-                }
-            }}
-        >
-            <TableCell />
-            <TableCell
+        <>
+            <TableRow
                 sx={{
-                    // TODO: Make this cell look like a title
-                    fontSize: "1.4rem",
+                    [`& .${tableCellClasses.root}`]: {
+                        borderBottom: "none"
+                    }
                 }}
-                colSpan={2}
             >
-                <ConditionalTextField
-                    onChange={onInputChange}
-                    condition={isRowEditing}
-                    name={"groupTitle"}
-                    value={groupTitle}
-                />
-            </TableCell>
-
-            {/* TODO: Add a search field */}
-            <TableCell
-                align="right"
-                colSpan={2}
-            >
-                <TextField
-                    // extra small(xs): none
-                    // small(sm): table-cell
-                    sx={{display: { xs: 'none', sm: 'inline-block' }}}
-                    id="outlined-search"
-                    label="Search"
-                    type="search"
-                    variant="outlined"
-                    size="small"
-                    InputProps={{
-                        endAdornment: (
-                            <IconButton>
-                                <SearchOutlined />
-                            </IconButton>
-                        )
+                <TableCell/>
+                <TableCell
+                    sx={{
+                        fontSize: "1.4rem",
                     }}
-                />
-                <Tooltip title={'Add new qr code'} >
-                    <IconButton onClick={handleAddEmptyRow}>
-                        <AddOutlined/>
-                    </IconButton>
-                </Tooltip>
+                    colSpan={2}
+                >
+                    { groupTitle }
+                </TableCell>
 
-                {
-                    isRowEditing
-                     ? <IconButton onClick={handleSaveRow}> <SaveOutlined /> </IconButton>
-                     : <Tooltip title={'Edit title'} ><IconButton onClick={handleEditRow}> <EditOutlined /> </IconButton></Tooltip>
-                }
-                <Tooltip title={'Delete group'}>
-                    <IconButton onClick={handleToggleDeleteDialog}>
-                        <DeleteOutlined/>
-                    </IconButton>
-                </Tooltip>
+                <TableCell
+                    align="right"
+                    colSpan={2}
+                >
+                    <TextField
+                        // extra small(xs): none
+                        // small(sm): table-cell
+                        sx={{display: {xs: 'none', sm: 'inline-block'}}}
+                        id="outlined-search"
+                        label="Search"
+                        type="search"
+                        variant="outlined"
+                        size="small"
+                        InputProps={{
+                            endAdornment: (
+                                <IconButton>
+                                    <SearchOutlined/>
+                                </IconButton>
+                            )
+                        }}
+                    />
+                    <Tooltip title={'Add new qr code'}>
+                        <IconButton onClick={handleAddEmptyRow}>
+                            <AddOutlined/>
+                        </IconButton>
+                    </Tooltip>
 
-                {
-                    isTryingToDelete && <ConfirmDialog onConfirm={handleDeleteRow} onCancel={handleToggleDeleteDialog} />
-                }
+                    <Tooltip title={'Edit title'}><IconButton onClick={toggleTitleModal}> <EditOutlined/>
+                    </IconButton></Tooltip>
 
-            </TableCell>
+                    <Tooltip title={'Delete group'}>
+                        <IconButton onClick={handleToggleDeleteDialog}>
+                            <DeleteOutlined/>
+                        </IconButton>
+                    </Tooltip>
 
-        </TableRow>
+                    {
+                        isTryingToDelete &&
+                        <ConfirmDialog onConfirm={handleDeleteRow} onCancel={handleToggleDeleteDialog}/>
+                    }
+
+                </TableCell>
+            </TableRow>
+
+            <TitleModal/>
+        </>
     )
 });
