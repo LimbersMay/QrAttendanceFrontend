3cbf29c0bd99a81d7fba0c06c1c90ead470e19ec
limberMay <limbermay1@gmail.com>
2023-02-18 18:04:30 -0600

    (qrAttendance) funciones para manejar las acciones del menú de opciones eliminadas de QrCodeRow

--- src/qrAttendance/components/qrCodeRow/QrCodeRow.tsx
+++ src/qrAttendance/components/qrCodeRow/QrCodeRow.tsx
@@ -1,4 +1,4 @@
-import {memo, useState} from "react";
+import {memo} from "react";
 
 import dayjs from "dayjs";
 import {IconButton, TableCell, TableRow} from "@mui/material";
@@ -6,41 +6,18 @@ import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import {QrCode, Registry} from "../../interfaces";
-import {useAppDispatch} from "../../../store";
-import {setActiveQrCode, startDeleteQrCodeWithDependencies} from "../../../store/qrAttendance";
 import {QrCodeMenuOptions} from "./QrCodeMenuOptions";
-import {SnackbarUtilities} from "../../../utilities/snackbar-manager";
 import {QrCheckIn} from "./QrCheckIn";
 import {generateExcelFromRegistries} from "../../helpers/generateExcelFromRegistries";
 import {QrCodeModal} from "./QrCodeModal";
-import {useUiSlice} from "../../../hooks/useUiSlice";
 
 export const QrCodeRow = memo(({
-           qrCodeRow,
-           handleOpenSubTable,
-           open,
-    registries
-}:{ qrCodeRow: QrCode, handleOpenSubTable: Function, open: boolean, registries: Registry[] }) => {
+                                   qrCodeRow,
+                                   handleOpenSubTable,
+                                   open,
+                                   registries
+                               }: { qrCodeRow: QrCode, handleOpenSubTable: Function, open: boolean, registries: Registry[] }) => {
 
-    const dispatch = useAppDispatch();
-
-    const { openQrCodeModal } = useUiSlice();
-
-    const [isQrShowing, setIsQrShowing] = useState<boolean>(false);
-
-    const handleEdit = () => {
-        openQrCodeModal()
-        dispatch(setActiveQrCode(qrCodeRow));
-    }
-
-    const handleDelete = () => {
-        dispatch(startDeleteQrCodeWithDependencies(qrCodeRow.id));
-        SnackbarUtilities.sucess(`QR Code ${qrCodeRow.name} deleted successfully`);
-    }
-
-    const handleToggleShowQr = () => {
-        setIsQrShowing(!isQrShowing);
-    }
 
     const handleDownload = () => {
         generateExcelFromRegistries(qrCodeRow,registries);
@@ -59,38 +36,25 @@ export const QrCodeRow = memo(({
                 </IconButton>
             </TableCell>
             <TableCell component="th" scope="row">
-                { qrCodeRow.name }
+                {qrCodeRow.name}
             </TableCell>
             <TableCell align="center">
                 {registries.length}
             </TableCell>
             <TableCell align="center" sx={{display: {xs: 'none', sm: 'table-cell'}}}>
-                { dayjs(qrCodeRow.date).format('DD/MM/YYYY') }
+                {dayjs(qrCodeRow.date).format('DD/MM/YYYY')}
             </TableCell>
             <TableCell align="center">
-                { qrCodeRow.enabled ? 'YES' : 'NO'}
+                {qrCodeRow.enabled ? 'YES' : 'NO'}
             </TableCell>
             <TableCell align="center">
-                {
-                    <QrCodeMenuOptions
-                        handleEdit={handleEdit}
-                        handleDelete={handleDelete}
-                        handleShow={handleToggleShowQr}
-                        handleDownload={handleDownload}
-                    />
-                }
+                <QrCodeMenuOptions
+                    handleDownload={handleDownload}
+                    qrCode={qrCodeRow}
+                />
             </TableCell>
 
-            {
-                isQrShowing
-                &&
-                <QrCheckIn
-                    isQrShowing={isQrShowing}
-                    url={`${qrCodeRow.url}/${qrCodeRow.formId}`}
-                    title={qrCodeRow.name}
-                    handleToggleShowQr={handleToggleShowQr}
-                />
-            }
+            <QrCheckIn/>
 
             <QrCodeModal/>
 
