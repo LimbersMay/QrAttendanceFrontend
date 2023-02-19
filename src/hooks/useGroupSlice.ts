ca04a843c8ac298c84e4ff965b6bdda66f9728b2
limberMay <limbermay1@gmail.com>
2023-02-19 13:03:03 -0600

    (qrAttendance) método updateGroup agregado al hook useGroupSlice

--- src/hooks/useGroupSlice.ts
+++ src/hooks/useGroupSlice.ts
@@ -1,13 +1,22 @@
 import {useAppDispatch, useAppSelector} from "../store";
-import {selectGroup} from "../store/qrAttendance";
+import {selectGroup, startUpdateGroup} from "../store/qrAttendance";
+import {Group} from "../qrAttendance/interfaces";
 
 export const useGroupSlice = () => {
 
     const dispatch = useAppDispatch();
     const { groups, active } = useAppSelector(selectGroup);
 
+    const updateGroup = (group: Group) => {
+        dispatch(startUpdateGroup(group));
+    }
+
     return {
+        // properties
         groups,
         active,
+
+        // methods
+        updateGroup
     }
 }
