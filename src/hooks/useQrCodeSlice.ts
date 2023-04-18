afb2e20cad26b5011ae533c9655b6214edc23066
limberMay <limbermay1@gmail.com>
2023-04-18 16:46:46 -0600

    (qrAttendance/hooks) Se movió el código asincrono de los thunks al hook useQrCodeSlice

--- src/hooks/useQrCodeSlice.ts
+++ src/hooks/useQrCodeSlice.ts
@@ -1,36 +1,84 @@
 import {useAppDispatch, useAppSelector} from "../store";
 import {
     selectQrCode,
-    setActiveQrCode,
-    startDeleteQrCodeWithDependencies, startNewQrCode,
-    startUpdateQrCode
+    onSetActiveQrCode,
+    updateQrCode, addEmptyQrCode, selectGroup, deleteQrCode, deleteRegistriesByQrCodeId
 } from "../store/qrAttendance";
 import {QrCode} from "../qrAttendance/interfaces";
 import {SnackbarUtilities} from "../utilities/snackbar-manager";
+import {qrAttendanceApi} from "../api/qrAttendanceApi";
 
 export const useQrCodeSlice = () => {
 
     const dispatch = useAppDispatch();
 
     const { activeQrCode, qrCodes } = useAppSelector(selectQrCode);
+    const { active } = useAppSelector(selectGroup);
 
-    const handleSetActiveQrCode = (qrCode: QrCode | null) => {
-        dispatch(setActiveQrCode(qrCode));
+    const setActiveQrCode = (qrCode: QrCode | null) => {
+        dispatch(onSetActiveQrCode(qrCode));
     }
 
-    const handleDeleteQrCode = () => {
-        dispatch(startDeleteQrCodeWithDependencies(`${activeQrCode?.id}`));
-        dispatch(setActiveQrCode(null));
+    const startDeleteQrCode = async () => {
+        const idQrCode = `${activeQrCode?.id}`;
+
+        // async code here
+        await qrAttendanceApi.delete(`/qrCode/delete/${idQrCode}`);
+
+        // sync code here
+        dispatch(deleteQrCode(idQrCode));
+        dispatch(deleteRegistriesByQrCodeId(idQrCode));
+
+        dispatch(onSetActiveQrCode(null));
         SnackbarUtilities.sucess(`QR Code ${activeQrCode?.name} deleted successfully`);
     }
 
-    const handleUpdateQrCode = (qrCode: QrCode) => {
-        dispatch(startUpdateQrCode(qrCode));
+    const startUpdateQrCode = async (qrCode: QrCode) => {
+
+        // async code here
+        const {id, name, enabled, date} = qrCode;
+
+        await qrAttendanceApi.put('/qrCode/update', {
+            id: id,
+            updatedFields: {
+                name,
+                enabled,
+                manualRegistrationDate: date
+            }
+        });
+
+        // sync code here
+        dispatch(updateQrCode(qrCode));
+
         SnackbarUtilities.sucess(`QR Code ${qrCode.name} updated successfully`);
     }
 
-    const handleStartNewQrCode = (name: string, date: string, enabled: boolean) => {
-        dispatch(startNewQrCode(name, date, enabled));
+    const startNewQrCode = async (name: string, manualRegistrationDate: string, enabled: boolean) => {
+
+        const id = active?.id || '';
+
+        const response = await qrAttendanceApi.post('/qrCode/create', {
+            groupId: id,
+            name,
+            enabled,
+            manualRegistrationDate,
+            url: 'https://qrattendancebackend.up.railway.app/checkIn'
+        });
+
+        const {body} = response.data;
+
+        const newQrCode: QrCode = {
+            groupId: id,
+            id: body.id,
+            url: body.url,
+            formId: body.formId,
+            name: body.name,
+            date: body.manualRegistrationDate,
+            enabled: false
+        }
+
+        // sync code here
+        dispatch(addEmptyQrCode(newQrCode));
     }
 
     return {
@@ -39,9 +87,9 @@ export const useQrCodeSlice = () => {
         qrCodes,
 
         // methods
-        handleSetActiveQrCode,
-        handleDeleteQrCode,
-        handleUpdateQrCode,
-        handleStartNewQrCode
+        setActiveQrCode,
+        startDeleteQrCode,
+        startUpdateQrCode,
+        startNewQrCode
     }
 }
