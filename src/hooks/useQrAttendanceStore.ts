3a2d34fa160478a3b3966429ee7d139123f59858
limberMay <limbermay1@gmail.com>
2023-04-21 23:58:35 -0600

    (qrAttendance/hooks) se pasó todo el código asíncrono del thunk del qrAttendance de las actions startDeleteGroupWithDependencies y startDeleteQrCodeWithDependencies

--- src/hooks/useQrAttendanceStore.ts
+++ src/hooks/useQrAttendanceStore.ts
@@ -1,20 +1,53 @@
-import {useAppDispatch} from "../store";
+import {useAppDispatch, useAppSelector} from "../store";
 import {QrCode} from "../qrAttendance/interfaces";
-import {startDeleteGroupWithDependencies} from "../store/qrAttendance";
+import {
+    deleteGroup, deleteQrCode,
+    deleteQrCodesByGroupId,
+    deleteRegistriesByQrCodeId, onSetActiveQrCode, selectQrCode
+} from "../store/qrAttendance";
+import { qrAttendanceApi } from "../api/qrAttendanceApi";
+import {SnackbarUtilities} from "../utilities/snackbar-manager";
 
 export const useQrAttendanceStore = () => {
 
     const dispatch = useAppDispatch();
+    const { activeQrCode } = useAppSelector(selectQrCode);
 
-    const deleteGroupWithDependencies = (groupId: string, qrCodes: QrCode[]) => {
-        dispatch(startDeleteGroupWithDependencies(groupId, qrCodes));
+    // TODO: Move all the code of the global thunks here, deleteGroupWithDependencies and deleteQrCodeWithDependencies
+    const startDeleteGroupWithDependencies = async (groupId: string, qrCodes: QrCode[]) => {
+
+        // async code here
+        await qrAttendanceApi.delete(`/group/delete/${groupId}`);
+
+        // sync code here
+        dispatch(deleteGroup(groupId));
+        dispatch(deleteGroup(groupId));
+        qrCodes.forEach(qrCode => {
+            dispatch(deleteRegistriesByQrCodeId(qrCode.id));
+        });
+        dispatch(deleteQrCodesByGroupId(groupId));
+    }
+
+    const startDeleteQrCodeWithDependencies = async () => {
+        const idQrCode = `${activeQrCode?.id}`;
+
+        // async code here
+        await qrAttendanceApi.delete(`/qrCode/delete/${idQrCode}`);
+
+        // sync code here
+        dispatch(deleteQrCode(idQrCode));
+        dispatch(deleteRegistriesByQrCodeId(idQrCode));
+
+        dispatch(onSetActiveQrCode(null));
+        SnackbarUtilities.sucess(`QR Code ${activeQrCode?.name} deleted successfully`);
     }
 
     return {
         // properties
 
         // methods
-        deleteGroupWithDependencies
+        startDeleteGroupWithDependencies,
+        startDeleteQrCodeWithDependencies
     }
 
 }
