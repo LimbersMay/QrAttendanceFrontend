0f66f8b25f37f0653d5f210f88d4ba45b872f77d
limberMay <limbermay1@gmail.com>
2023-06-05 18:37:42 -0600

    (test/fixtures groupStates) fixtures withGroupState agregado y elementos ficticios añadidos a la variable groups

--- tests/fixtures/groupStates.ts
+++ tests/fixtures/groupStates.ts
@@ -1,9 +1,30 @@
 import {Group} from "../../src/qrAttendance/interfaces";
 import {GroupState} from "../../src/store/qrAttendance";
 
-const groups: Group[] = []
+export const groups: Group[] = [
+    {
+        id: '1',
+        date: '2021-01-01',
+        name: 'Soft 1'
+    },
+    {
+        id: '2',
+        date: '2021-01-02',
+        name: 'Admin 1'
+    },
+    {
+        id: '3',
+        date: '2021-01-03',
+        name: 'Cont 2'
+    }
+]
 
 export const initialState: GroupState = {
     active: null,
     groups: []
+}
+
+export const withGroupsState = {
+    active: null,
+    groups: [...groups]
 }
