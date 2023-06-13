14397cc4e727ba8c9531a67a7077de5ccdc84213
limbermay <limbermay1@gmail.com>
2023-06-13 10:37:17 -0600

    (tests/store/ui uiSlice.test.ts) tests de onToggleQrCodeModal to false y onToggleQrCodeModal to true implementados

--- tests/store/ui/uiSlice.test.ts
+++ tests/store/ui/uiSlice.test.ts
@@ -1,5 +1,6 @@
 import clearAllMocks = jest.clearAllMocks;
-import {onHideQrCode, onShowQrCode, uiSlice} from "../../../src/store/ui/uiSlice";
+import {onHideQrCode, onShowQrCode, onToggleQrCodeModal, uiSlice} from "../../../src/store/ui/uiSlice";
+import {qrCodeModalOpenedState} from "../../fixtures/uiStates";
 
 describe('Tests for uiSlice', () => {
     beforeEach(() => clearAllMocks());
@@ -17,12 +18,22 @@ describe('Tests for uiSlice', () => {
     });
 
     test('onShowQrCode should set the isShowingQrCode on true', () => {
-        const newState = uiSlice.reducer(initialState, onShowQrCode());
-        expect(newState.isShowingQrCode).toBeTruthy();
+        const state = uiSlice.reducer(initialState, onShowQrCode());
+        expect(state.isShowingQrCode).toBeTruthy();
     });
 
     test('onHideQrCode should set the isShowingQrCode on false', () => {
-        const newState = uiSlice.reducer(initialState, onHideQrCode());
-        expect(newState.isShowingQrCode).toBeFalsy();
+        const state = uiSlice.reducer(initialState, onHideQrCode());
+        expect(state.isShowingQrCode).toBeFalsy();
+    });
+
+    test('onToggleQrCodeModal should set the isQrCodeModalOpen on true', () => {
+        const state = uiSlice.reducer(initialState, onToggleQrCodeModal());
+        expect(state.isQrCodeModalOpen).toBeTruthy();
+    });
+
+    test('onToggleQrCodeModal should set the isQrCodeModalOpen on false', () => {
+        const state = uiSlice.reducer(qrCodeModalOpenedState, onToggleQrCodeModal());
+        expect(state.isQrCodeModalOpen).toBeFalsy();
     });
 });
