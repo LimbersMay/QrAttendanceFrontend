1a2e24091d0e69cc69f9f1c27afe8cebaaa5f6cb
limbermay <limbermay1@gmail.com>
2023-06-13 08:41:10 -0600

    (tests/fixtures userStates.ts) fixtures userCredentials y notAuthenticatedState creados

---
+++ tests/fixtures/userStates.ts
@@ -0,0 +1,16 @@
+import {authStatusTypes} from "../../src/auth/types";
+import {AuthState} from "../../src/store/auth";
+
+export const userCredentials = {
+    displayName: "Limbers",
+    email: "limber@google.com",
+    uid: "12453"
+}
+
+export const notAuthenticatedState: AuthState = {
+    status: authStatusTypes.checking,
+    uid: null,
+    email: null,
+    displayName: null,
+    errorMessage: null
+}
