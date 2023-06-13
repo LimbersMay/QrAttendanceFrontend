5818c3e98fc1055be3229d451d943dcf8dbeabb2
limbermay <limbermay1@gmail.com>
2023-06-13 08:53:43 -0600

    (store/auth authSlice) test del logout sin errores implementado

--- tests/store/auth/authSlice.test.ts
+++ tests/store/auth/authSlice.test.ts
@@ -1,6 +1,6 @@
 import clearAllMocks = jest.clearAllMocks;
-import {authSlice, login} from "../../../src/store/auth";
-import {notAuthenticatedState, userCredentials} from "../../fixtures/userStates";
+import {authSlice, login, logout} from "../../../src/store/auth";
+import {authenticatedUserState, notAuthenticatedState, userCredentials} from "../../fixtures/userStates";
 import {authStatusTypes} from "../../../src/auth/types";
 
 describe('Tests for authSlice', () => {
@@ -18,6 +18,17 @@ describe('Tests for authSlice', () => {
             uid: userCredentials.uid,
             email: userCredentials.email,
             displayName: userCredentials.displayName,
+            errorMessage: null
+        });
+    });
+
+    test('should logout the user without error message', () => {
+        const state = authSlice.reducer(authenticatedUserState, logout(null));
+        expect(state).toEqual({
+            displayName: null,
+            email: null,
+            uid: null,
+            status: authStatusTypes.notAuthenticated,
             errorMessage: null
         });
     });
