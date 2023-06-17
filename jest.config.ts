7bd3a261aa0eef27a8b060ae45828a8aa9e4de64
limbermay <limbermay1@gmail.com>
2023-06-17 16:52:17 -0600

    (jest.env.ts) objeto global setInmediate agregado

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,13 @@
 export default {
-    testEnvironment: "jsdom",
+    testEnvironment: "jest-environment-jsdom",
     transform: {
         "^.+\\.tsx?$": "ts-jest"
     },
     setupFiles: ['./jest.setup.ts'],
     moduleNameMapper: {
         '\\.(css|less|sass|scss)$': 'identity-obj-proxy'
-    }
+    },
+    setupFilesAfterEnv: [
+        '<rootDir>/jest.env.ts',
+    ]
 }
