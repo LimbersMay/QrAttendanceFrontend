422e1510dd480e645a036fda6ef3b8bf9c931669
limbermay <limbermay1@gmail.com>
2023-06-20 12:14:56 -0600

    (tests/hooks useUiStore.test.tsx) await agregado a cada llamada a act

--- tests/hooks/useUiStore.test.tsx
+++ tests/hooks/useUiStore.test.tsx
@@ -46,112 +46,112 @@ describe('Tests for useUiStore', () => {
         });
     });
 
-    test('showQrCode should set the isShowingQrCode to true', () => {
+    test('showQrCode should set the isShowingQrCode to true', async () => {
         const mockStore = getMockStore();
 
         const { result } = renderHook(() => useUiStore(), {
             wrapper: ({children}) => <Provider store={mockStore}>{children}</Provider>
         });
 
-        act(() => {
+        await act(() => {
             result.current.showQrCode();
         });
 
         expect(result.current.isShowingQrCode).toBeTruthy();
     });
 
-    test('hideQrCode should set the isShowingQrCode to false', () => {
+    test('hideQrCode should set the isShowingQrCode to false', async () => {
         const mockStore = getMockStore({...withShowingQrCodeTrue});
 
         const { result } = renderHook(() => useUiStore(), {
             wrapper: ({children}) => <Provider store={mockStore}>{children}</Provider>
         });
 
-        act(() => {
+       await act(() => {
             result.current.hideQrCode();
         });
 
         expect(result.current.isShowingQrCode).toBeFalsy();
     });
 
-    test('toggleQrCodeModal should set the isQrCodeModalOpen to true from false', () => {
+    test('toggleQrCodeModal should set the isQrCodeModalOpen to true from false',async () => {
         const mockStore = getMockStore({...initialState});
 
         const { result } = renderHook(() => useUiStore(), {
             wrapper: ({children}) => <Provider store={mockStore}>{children}</Provider>
         });
 
-        act(() => {
+        await act(() => {
             result.current.toggleQrCodeModal();
         });
 
         expect(result.current.isQrCodeModalOpen).toBeTruthy();
     });
 
-    test('toggleQrCodeModal should set the isQrCodeModalOpen to false from true', () => {
+    test('toggleQrCodeModal should set the isQrCodeModalOpen to false from true', async () => {
         const mockStore = getMockStore({...withQrCodeModalOpenTrue});
 
         const { result } = renderHook(() => useUiStore(), {
             wrapper: ({children}) => <Provider store={mockStore}>{children}</Provider>
         });
 
-        act(() => {
+        await act(() => {
             result.current.toggleQrCodeModal();
         });
 
         expect(result.current.isQrCodeModalOpen).toBeFalsy();
     });
 
-    test('toggleRegistryModal should set the isRegistryModalOpen to true from false', () => {
+    test('toggleRegistryModal should set the isRegistryModalOpen to true from false', async () => {
         const mockStore = getMockStore({...initialState});
 
         const { result } = renderHook(() => useUiStore(), {
             wrapper: ({children}) => <Provider store={mockStore}>{children}</Provider>
         });
 
-        act(() => {
+        await act(() => {
             result.current.toggleRegistryModal();
         });
 
         expect(result.current.isRegistryModalOpen).toBeTruthy();
     });
 
-    test('toggleRegistryModal should set the isRegistryModalOpen to false from true', () => {
+    test('toggleRegistryModal should set the isRegistryModalOpen to false from true', async () => {
         const mockStore = getMockStore({...withRegistryModalOpenTrue});
 
         const { result } = renderHook(() => useUiStore(), {
             wrapper: ({children}) => <Provider store={mockStore}>{children}</Provider>
         });
 
-        act(() => {
+        await act(() => {
             result.current.toggleRegistryModal();
         });
 
         expect(result.current.isRegistryModalOpen).toBeFalsy();
     });
 
-    test('toggleTitleModal should set the isRegistryModalOpen to true from false', () => {
+    test('toggleTitleModal should set the isRegistryModalOpen to true from false', async () => {
         const mockStore = getMockStore({...initialState});
 
         const { result } = renderHook(() => useUiStore(), {
             wrapper: ({children}) => <Provider store={mockStore}>{children}</Provider>
         });
 
-        act(() => {
+        await act(() => {
             result.current.toggleTitleModal();
         });
 
         expect(result.current.isTitleModalOpen).toBeTruthy();
     });
 
-    test('toggleTitleModal should set the isRegistryModalOpen to false from true', () => {
+    test('toggleTitleModal should set the isRegistryModalOpen to false from true', async () => {
         const mockStore = getMockStore({...withTitleModalOpenTrue});
 
         const { result } = renderHook(() => useUiStore(), {
             wrapper: ({children}) => <Provider store={mockStore}>{children}</Provider>
         });
 
-        act(() => {
+        await act(() => {
             result.current.toggleTitleModal();
         });
 
