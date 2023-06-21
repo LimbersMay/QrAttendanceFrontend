54c8833fb656d552b0eb5116ee62980132187f49
limbermay <limbermay1@gmail.com>
2023-06-21 06:42:30 -0600

    (tests/fixtures testUser.ts) objeto testUserCredentials creado para simular las credenciales de un usuario

---
+++ tests/fixtures/testUser.ts
@@ -0,0 +1,8 @@
+
+export const testUserCredentials = {
+    email: 'test@google.com',
+    password: '123456',
+    id: '640b33dc771579718254417a',
+    name: 'Test User',
+    lastname: 'May Ek'
+}
