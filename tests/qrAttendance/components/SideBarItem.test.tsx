da8981f5c65524d3d2c077743b549cfcbed3d201
limbermay <limbermay1@gmail.com>
2023-07-11 14:15:47 -0600

    (tests/qrAttendance/components SideBarItem.test.tsx ) test inicial y de enviar el grupo correcto a la función setActiveGroup implementados

---
+++ tests/qrAttendance/components/SideBarItem.test.tsx
@@ -0,0 +1,34 @@
+import {groups} from "../../fixtures/qrAttendanceStates";
+import {fireEvent, render, screen} from "@testing-library/react";
+import {SideBarItem} from "../../../src/qrAttendance/components/SideBarItem";
+import {useGroupStore} from "../../../src/hooks/useGroupStore";
+
+jest.mock("../../../src/hooks/useGroupStore");
+
+describe('Tests for <SideBarItem />', () => {
+
+    const mockSetActiveGroup = jest.fn();
+
+    test('should render the component successfully', async () => {
+
+        const {container} = await render(
+            <SideBarItem group={groups[0]}/>
+        );
+
+        expect(container).toMatchSnapshot();
+    });
+
+    test('should send the selected group when clicking on a card', () => {
+
+        (useGroupStore as jest.Mock).mockReturnValue({
+            setActiveGroup: mockSetActiveGroup
+        });
+
+        render(<SideBarItem group={groups[0]}/>);
+
+        const groupItem = screen.getByTestId('group-item');
+        fireEvent.click(groupItem);
+
+        expect(mockSetActiveGroup).toHaveBeenCalledWith(groups[0]);
+    });
+});
