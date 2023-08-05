2cdd887556d73573481b4d162225b61beb241ddb
LimbersMay <limbermay1@gmail.com>
2023-08-05 12:42:37 -0600

    (src/store/checkInForm thunks.ts) thunks eliminados

--- src/store/checkInForm/thunks.ts
+++
@@ -1,8 +0,0 @@
-import {AppThunk} from "../store";
-import {FormType} from "../../checkInForm/types/formType";
-
-export const startSubmitCheckInForm = ({ io, name, group, career, firstSurname, secondSurname, formId }: FormType): AppThunk => {
-    return (dispatch) => {
-        io.emit('register-attendance', { name, group, career, firstSurname, secondSurname, formId });
-    }
-}
