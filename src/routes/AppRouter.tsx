d3f1029a7a30342865622af480936951a54babcb
LimbersMay <limbermay1@gmail.com>
2023-08-06 18:28:18 -0600

    token del servidor obtenido en la queryParam y se reemplazo por un string vacio

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,12 +1,20 @@
-import {Routes, Route, Navigate} from "react-router-dom";
+import {Routes, Route, Navigate, useSearchParams} from "react-router-dom";
 import {AuthRoutes} from "../auth/routes/AuthRoutes";
 import {QrAttendanceRoutes} from "../qrAttendance/routes/QrAttendanceRoutes";
-import {useCheckAuth} from "../hooks/useCheckAuth";
+import {useCheckAuth} from "../hooks";
 import {authStatusTypes} from "../auth/types";
 import {CheckingAuth} from "../iu";
 import {CheckInFormRouter} from "../checkInForm/CheckInFormRouter";
 
 export const AppRouter = () => {
+
+    const [query] = useSearchParams();
+    const token = query.get('token');
+
+    if (token) {
+        localStorage.setItem('token', token);
+        window.location.href = '/';
+    }
 
     const status = useCheckAuth();
 
