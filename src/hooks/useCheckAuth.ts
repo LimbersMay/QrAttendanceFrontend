59c08a225df1a9f7303af9a89aaae57e1cd1adb1
LimbersMay <limbermay1@gmail.com>
2023-08-09 11:55:51 -0600

    (useAuthStore.ts) se obtuvo el objeto user desde la response.data en lugar del body

--- src/hooks/useCheckAuth.ts
+++ src/hooks/useCheckAuth.ts
@@ -14,8 +14,8 @@ export const useCheckAuth = () => {
         qrAttendanceApi.get("/auth/authenticated", {
             withCredentials: true
         }).then((response) => {
-            const { body: user } = response.data;
-            
+
+            const { user } = response.data;
 
             if (!user) {
                 return dispatch(logout(null));
